e8d126dba8101cc1b2c8d93dace3f0151e5af314
bakbassjunior <bakbassjunior@gmail.com>
2026-09-24 18:32:27 +0000

    Passe l'URL de l'API en HTTPS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.athl.athl-logistique.com',
+  apiUrl: 'https://api.athl.athl-logistique.com',
   cloudinary: {
     cloudName: 'drfq0bt4z',
     uploadPreset: 'xbanking',
